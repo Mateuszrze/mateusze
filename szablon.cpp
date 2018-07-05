ef24cb645315600d969872a6484fa1f55fd6f58b
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2018-07-05 09:40:47 +0200

    Update szablon.cpp

--- szablon.cpp
+++ szablon.cpp
@@ -1,8 +1,14 @@
+//~ while (clock()<=69*CLOCKS_PER_SEC)
+//~ #pragma comment(linker, "/stack:200000000")
+//~ #pragma GCC optimize("O3")
+//~ #pragma GCC optimize("Ofast")
+//~ #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//~ #pragma GCC optimize("unroll-loops")
 #ifdef LOCAL
-#define DBG(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#define dbg(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
 #define _GLIBCXX_DEBUG
 #else
-#define DBG(...)
+#define dbg(...)
 #endif
 
 #include<bits/stdc++.h>
@@ -11,15 +17,16 @@
 #define pb push_back
 #define fi first
 #define se second
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef long double LD;
-const LL INF = 1e18L + 1;
-const int inf = 1e9 + 1;
-const LL mod=1e9+7;
 
 using namespace std;
 
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+const LL INF = 1e18L + 7;
+const int inf = 1e9 + 7;
+const LL mod = 1e9+7;
+const LD eps = 1e-9;
 
 template<class TH> void _dbg(const char *sdbg, TH h){ cerr<<sdbg<<'='<<h<<endl; }
 template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
@@ -27,12 +34,12 @@ template<class TH, class... TA> void _dbg(const char *sdbg, TH h, TA... a) {
   cerr<<'='<<h<<','; _dbg(sdbg+1, a...);
 }
 
-
 int main()
 {
 #ifndef LOCAL
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+    cout.tie(0);
 #endif
 
     return 0;
