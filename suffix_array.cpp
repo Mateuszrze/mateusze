0f4114ce1286db959233a19bdbb037aeffe149a5
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2018-07-08 21:18:29 +0200

    Create suffix_array.cpp

---
+++ suffix_array.cpp
@@ -0,0 +1,173 @@
+
+///////////////// O(n)
+// 1e6 - 0,2 s
+// 2e6 - 0,4 s
+// 4e6 - 2 s
+// 8e6 - 5 s
+
+const int N = 1e7 + 7; // rozmiar stringa
+
+inline bool leq(int a1, int a2,   int b1, int b2) { // lexic. order for pairs
+  return(a1 < b1 || a1 == b1 && a2 <= b2);
+}                                                   // and triples
+inline bool leq(int a1, int a2, int a3,   int b1, int b2, int b3) {
+  return(a1 < b1 || a1 == b1 && leq(a2,a3, b2,b3));
+}
+
+// stably sort a[0..n-1] to b[0..n-1] with keys in 0..K from r
+static void radixPass(int* a, int* b, int* r, int n, int K)
+{ // count occurrences
+  int* c = new int[K + 1];                          // counter array
+  for (int i = 0;  i <= K;  i++) c[i] = 0;         // reset counters
+  for (int i = 0;  i < n;  i++) c[r[a[i]]]++;    // count occurences
+  for (int i = 0, sum = 0;  i <= K;  i++) { // exclusive prefix sums
+     int t = c[i];  c[i] = sum;  sum += t;
+  }
+  for (int i = 0;  i < n;  i++) b[c[r[a[i]]]++] = a[i];      // sort
+  delete [] c;
+}
+
+// find the suffix array SA of s[0..n-1] in {1..K}^n
+// require s[n]=s[n+1]=s[n+2]=0, n>=2
+void suffixArray(int* s, int* SA, int n, int K) {
+  int n0=(n+2)/3, n1=(n+1)/3, n2=n/3, n02=n0+n2;
+  int* s12  = new int[n02 + 3];  s12[n02]= s12[n02+1]= s12[n02+2]=0;
+  int* SA12 = new int[n02 + 3]; SA12[n02]=SA12[n02+1]=SA12[n02+2]=0;
+  int* s0   = new int[n0];
+  int* SA0  = new int[n0];
+
+  // generate positions of mod 1 and mod  2 suffixes
+  // the "+(n0-n1)" adds a dummy mod 1 suffix if n%3 == 1
+  for (int i=0, j=0;  i < n+(n0-n1);  i++) if (i%3 != 0) s12[j++] = i;
+
+  // lsb radix sort the mod 1 and mod 2 triples
+  radixPass(s12 , SA12, s+2, n02, K);
+  radixPass(SA12, s12 , s+1, n02, K);
+  radixPass(s12 , SA12, s  , n02, K);
+
+  // find lexicographic names of triples
+  int name = 0, c0 = -1, c1 = -1, c2 = -1;
+  for (int i = 0;  i < n02;  i++) {
+    if (s[SA12[i]] != c0 || s[SA12[i]+1] != c1 || s[SA12[i]+2] != c2) {
+      name++;  c0 = s[SA12[i]];  c1 = s[SA12[i]+1];  c2 = s[SA12[i]+2];
+    }
+    if (SA12[i] % 3 == 1) { s12[SA12[i]/3]      = name; } // left half
+    else                  { s12[SA12[i]/3 + n0] = name; } // right half
+  }
+
+  // recurse if names are not yet unique
+  if (name < n02) {
+    suffixArray(s12, SA12, n02, name);
+    // store unique names in s12 using the suffix array
+    for (int i = 0;  i < n02;  i++) s12[SA12[i]] = i + 1;
+  } else // generate the suffix array of s12 directly
+    for (int i = 0;  i < n02;  i++) SA12[s12[i] - 1] = i;
+
+  // stably sort the mod 0 suffixes from SA12 by their first character
+  for (int i=0, j=0;  i < n02;  i++) if (SA12[i] < n0) s0[j++] = 3*SA12[i];
+  radixPass(s0, SA0, s, n0, K);
+
+  // merge sorted SA0 suffixes and sorted SA12 suffixes
+  for (int p=0,  t=n0-n1,  k=0;  k < n;  k++) {
+#define GetI() (SA12[t] < n0 ? SA12[t] * 3 + 1 : (SA12[t] - n0) * 3 + 2)
+    int i = GetI(); // pos of current offset 12 suffix
+    int j = SA0[p]; // pos of current offset 0  suffix
+    if (SA12[t] < n0 ?
+        leq(s[i],       s12[SA12[t] + n0], s[j],       s12[j/3]) :
+        leq(s[i],s[i+1],s12[SA12[t]-n0+1], s[j],s[j+1],s12[j/3+n0]))
+    { // suffix from SA12 is smaller
+      SA[k] = i;  t++;
+      if (t == n02) { // done --- only SA0 suffixes left
+        for (k++;  p < n0;  p++, k++) SA[k] = SA0[p];
+      }
+    } else {
+      SA[k] = j;  p++;
+      if (p == n0)  { // done --- only SA12 suffixes left
+        for (k++;  t < n02;  t++, k++) SA[k] = GetI();
+      }
+    }
+  }
+  delete [] s12; delete [] SA12; delete [] SA0; delete [] s0;
+}
+
+
+int input[N+5];
+int SA[N+5];
+string s;
+int n;
+
+vector<int> lcp(const string& s, int* sa)
+{
+    int n=s.size(),k=0;
+    vector<int> lcp(n,0);
+    vector<int> rank(n,0);
+
+    for(int i=0; i<n; i++) rank[sa[i]]=i;
+
+    for(int i=0; i<n; i++, k?k--:0)
+    {
+        if(rank[i]==n-1) {k=0; continue;}
+        int j=sa[rank[i]+1];
+        while(i+k<n && j+k<n && s[i+k]==s[j+k]) k++;
+        lcp[rank[i]]=k;
+    }
+    return lcp;
+}
+
+
+int main()
+{
+  cin>>s;
+  for(int i=0;i<s.size();i++) input[i] = s[i] - 'a' + 1;
+  n = (int) s.size();
+  input[n] = 0; input[n+1] = 0; input[n+2] = 0;
+  suffixArray(input, SA, n, 26);
+}
+
+///////////////// O(n log(n) ^ 2)
+// 1e6 - 0,5 s
+// 2e6 - 2,5 s
+// 4e6 - 5 s
+// 8e6 - 12 s
+// how to use
+// S ustaw na wskaznik na poczatek stringa
+// buildSA() - w taclicy sa znajduje sie suffixarray tego ciagu
+// buildLCP() - musi byc wczesniej odpalony buildSA(), w ciagu lcp znajduje sie lcp
+
+const int MAXN = 1 << 20; // maksymalna dlugosc stringa
+char * S;
+int N, gap;
+int sa[MAXN], pos[MAXN], tmp[MAXN], lcp[MAXN];
+
+bool sufCmp(int i, int j)
+{
+	if (pos[i] != pos[j])
+		return pos[i] < pos[j];
+	i += gap;
+	j += gap;
+	return (i < N && j < N) ? pos[i] < pos[j] : i > j;
+}
+
+void buildSA()
+{
+	N = strlen(S);
+	for(int i = 0 ;i < N;i++) sa[i] = i, pos[i] = S[i];
+	for (gap = 1;; gap *= 2)
+	{
+		sort(sa, sa + N, sufCmp);
+		for(int i = 0 ;i < N - 1;i ++) tmp[i + 1] = tmp[i] + sufCmp(sa[i], sa[i + 1]);
+		for(int i = 0 ;i < N ;i++) pos[sa[i]] = tmp[i];
+		if (tmp[N - 1] == N - 1) break;
+	}
+}
+
+void buildLCP()
+{
+	for (int i = 0, k = 0; i < N; ++i) if (pos[i] != N - 1)
+	{
+		for (int j = sa[pos[i] + 1]; S[i + k] == S[j + k];)
+		++k;
+		lcp[pos[i]] = k;
+		if (k)--k;
+	}
+}
