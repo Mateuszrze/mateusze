f69e8a81da809b010e0d4f23d182fc66eff394b5
Mordacz <39000271+Mordacz@users.noreply.github.com>
2018-07-18 01:12:32 +0200

    Add files via upload

---
+++ kmp.cpp
@@ -0,0 +1,18 @@
+
+
+struct kmp
+{
+	void licz(char * S, int * P)
+	{
+		int len = strlen(S), p = 0;
+		P[0] = -1;
+		for(int i = 1; i < len; ++i)
+		{
+			while(p >= 0 && S[i] != S[p])
+				p = P[p];
+			++p;
+			P[i + 1] = p;
+		}
+	}
+};
+
