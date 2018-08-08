ad63488038d0b6c278a0fd08bfc7c45bdcaa6098
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2018-08-08 20:25:27 +0200

    Update geo.cpp

--- geo.cpp
+++ geo.cpp
@@ -65,3 +65,36 @@ point inter(line a, line b)
 		return {1e18, 1e18}; // te proste sie nie przecinaja
 	return {(b.b * a.c - a.b * b.c ) / (b.a * a.b - a.a * b.b), (b.a * a.c - a.a * b.c) / (a.a * b.b - b.a * a.b)};
 }
+
+bool com(point a, point b)
+{
+	if(a.x != b.x)
+		return a.x < b.x;
+	return a.y < b.y;
+}
+
+// zwraca otoczke wypukla zbioru punktow cur
+// uwaga na zmiennoprzecinkowe 
+
+vector<point> rob(vector<point> cur)
+{
+	sort(all(cur), com);
+	vector<point> oto;
+	for(auto s : cur)
+	{
+		while(SZ(oto) >= 2 && wyzznak(oto[SZ(oto) - 2], oto.back(), s) <= 0)
+			oto.pop_back();
+		oto.push_back(s);
+	}
+	reverse(all(cur));
+	vector<point> oto2;
+	for(auto s : cur)
+	{
+		while(SZ(oto2) >= 2 && wyzznak(oto2[SZ(oto2) - 2], oto2.back(), s) <= 0)
+			oto2.pop_back();
+		oto2.push_back(s);
+	}
+	for(int i = 1; i < SZ(oto2) - 1;i++)
+		oto.pb(oto2[i]);	
+	return oto;
+}
