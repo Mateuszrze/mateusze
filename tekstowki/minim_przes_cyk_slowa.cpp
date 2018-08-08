f921c7ac3b86656fc1f8d7bcad6425749c0a8233
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2018-08-08 12:07:42 +0200

    Create minim_przes_cyk_slowa.cpp

---
+++ tekstowki/minim_przes_cyk_slowa.cpp
@@ -0,0 +1,64 @@
+//wsyzstko dziala w czasie liniowym
+//Lyndon_fact to podzial s na w_1, w_2, w_3, ..., w_k (s = w_1 + ... + w_k) takie, ze;
+//w_1 >= w_2 >= ... >= w_k (leksykograficznie)
+//w sa slowami Lyndona(sa swoimi scisle najwiekszymi przesunieciami cyklicznymi
+
+vector < string > Lyndon_fact(const string& s)
+{
+	int start = 0, p = 0, m = 1;
+	vector < string > odp;
+	while(start < SZ(s))
+	{
+		if (m == SZ(s) || s[p] > s[m])
+		{
+			int d = m-p;
+			while(start <= p)
+			{
+				odp.push_back(string(s, start, d));
+				start += d;
+			}
+			p = start;
+			m = p+1;
+			continue;
+		}
+		if (s[p] == s[m])
+			p++;
+		else
+			p = start;
+		m++;
+	}
+	return odp;
+}
+
+// zwraca pozycje litery od ktorej zaczyna sie minimalne przesuniecie leksykograficzne slowa
+
+int mlpc(string s)
+{
+	int roz = SZ(s), pop = 0, ind = 1, a;
+	s += s;
+	auto v = Lyndon_fact(s);
+	string ost = v[0];
+	a = SZ(v[0]);
+	while(a < roz)
+	{	
+		if (v[ind] != ost)
+			pop = a;
+		ost = v[ind];
+		a += SZ(v[ind]);
+		ind++;
+	}
+	return pop;
+}
+
+// zwraca slowo
+
+string mlpcslo(string s)
+{
+	int pos = mlpc(s);
+	string v;
+	for(int i = pos;i < SZ(s);i++)
+		v.pb(s[i]);
+	for(int i = 0 ; i< pos;i++)
+		v.pb(s[i]);
+	return v;
+}
