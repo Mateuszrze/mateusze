d83c3bd96fe7a6269829bde1d705d1539401e8ac
Mateuszrze <39000168+Mateuszrze@users.noreply.github.com>
2018-10-31 10:11:55 +0100

    Create wzorzec.cpp

---
+++ wzorzec.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+#define all(v) (v).begin(), (v).end()
+#define sz(v) (int)((v).size())
+#define pb push_back
+#define fi first
+#define se second
+
+using namespace std;
+
+typedef long long LL;
+typedef long double LD;
+typedef pair<int, int> PII;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef LOCAL
+  // to co ma sie dziac na sprawdzarce
+#elseif
+  // to co ma sie dziac u nas
+ #endif
+
+    return 0;
+}
