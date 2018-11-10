0adcdab94b02f7fd018f1723de1951c51691e9e1
Mordacz <39000271+Mordacz@users.noreply.github.com>
2018-11-10 18:22:49 +0100

    Update kmp.cpp

--- tekstowki/kmp.cpp
+++ tekstowki/kmp.cpp
@@ -1,18 +1,16 @@
-
-
 struct kmp
 {
 	void licz(char * S, int * P)
 	{
 		int len = strlen(S), p = 0;
 		P[0] = -1;
-		for(int i = 1; i < len; ++i)
+		P[1] = 0;
+		for(int i = 2; i <= len; ++i)
 		{
-			while(p >= 0 && S[i] != S[p])
+			while(p >= 0 && S[i - 1] != S[p])
 				p = P[p];
 			++p;
 			P[i] = p;
 		}
 	}
 };
-
